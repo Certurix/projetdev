f608ad93a7f26dd1bae0a0c5b3caa4ba710b6eda
Certurix <46199479+Certurix@users.noreply.github.com>
2023-06-03 20:25:48 +0200

    Remade the whole website

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>ProjetDev</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="ProjetDev est un studio indépendant créant des expériences de jeu sur la plateforme ROBLOX." />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
