8028fd3f53a358f8bdc92932d3d1e92ccc1362f7
Certurix <46199479+Certurix@users.noreply.github.com>
2023-06-03 14:02:17 +0200

    add login page

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/layout';
+
+export default function Login() {
+  return (
+    <Layout>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <h1>Désolé la page de connexion est en construction.</h1>
+    </Layout>
+  );
+}
