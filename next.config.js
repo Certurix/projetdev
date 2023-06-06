5642fa9d03134f303dc6780c9e7ca735bab0c469
Certurix <46199479+Certurix@users.noreply.github.com>
2023-06-06 21:11:10 +0200

    Added new include parameter

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/freenitro',
+        destination: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
