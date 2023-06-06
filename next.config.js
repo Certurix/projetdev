60688cc959eb6793a6277df68a688eda7b662cdd
Certurix <46199479+Certurix@users.noreply.github.com>
2023-06-06 21:00:33 +0200

    reset next config

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId },
-  ) {
-    return {
-      '/': { page: '/app/page.tsx' },
-      '/about': { page: '/app/about/page.tsx' },
-    };
-  },
 };
 
 module.exports = nextConfig;
