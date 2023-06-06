2ae305b490f3ae13fb092b16f64b7d3b19241a4c
Certurix <46199479+Certurix@users.noreply.github.com>
2023-06-06 09:04:14 +0000

    testing rewriting rules

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   experimental: {
     appDir: true,
+  },
+  /* Rewriting next rules*/
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/app/page",
+      },
+      {
+        source: "/about",
+        destination: "/app/about/page",
+      },
+    ];
   },
 };
 
